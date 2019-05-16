424bee208440c006803804ef31214e3e7eb518c6
richietam <richardtam.ny@gmail.com>
2019-05-16 14:34:49 -0400

    profile components implemented

--- src/component/ProfileCard.js
+++ src/component/ProfileCard.js
@@ -1,22 +1,39 @@
 import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
 
 const ProfileCard = () => (
   <Card id="ProfileCard">
     <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
     <Card.Content>
+      <Button
+        id="ProfileCardButton"
+      >
+        Follow Matthew
+      </Button>
       <Card.Header>Matthew</Card.Header>
       <Card.Meta>
         <span className='date'>Joined in 2015</span>
       </Card.Meta>
       <Card.Description>
-        Matthew is a musician living in Nashville.
+        I like turtles.
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
       <a>
         <Icon name='user' />
-        22 Friends
+        24,225 Followers
+      </a>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='hand lizard' />
+        2,312 Following
+      </a>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='camera retro' />
+        22,222 Photos Uploaded
       </a>
     </Card.Content>
   </Card>
--- src/container/Profile.js
+++ src/container/Profile.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Grid, Image, Header, Button } from 'semantic-ui-react'
 import ProfileMainImage from '../component/ProfileMainImage'
+import NewsFeed from '../component/NewsFeed'
 import ProfileCard from '../component/ProfileCard'
-import { Grid, Image, Header } from 'semantic-ui-react'
+import GalleryCards from '../component/GalleryCards'
 
 class Profile extends Component {
   render () {
     return (
       <div id="profile">
-        <ProfileMainImage />
+      <Grid>
+        <Grid.Column width={16}>
+          <ProfileMainImage />
+        </Grid.Column>
+      </Grid>
+
 
       <Grid>
         <Grid.Column width={5}>
-          <ProfileCard/> 
+          <ProfileCard/>
         </Grid.Column>
 
-        <Grid.Column width={8}>
+        <Grid.Column
+          width={11}
+          id="RedBorder"
+        >
+          <NewsFeed />
 
         </Grid.Column>
+      </Grid>
 
-        <Grid.Column width={3}>
-          <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+      <Grid>
+        <Grid.Column
+          width={16}
+          id="GalleryCardsGrid"
+        >
+          <Header as='h2'>Matthew's Galleries:</Header>
+          <div id="NewGalleryButton">
+            <Button>
+              New Gallery
+            </Button>
+          </div>
+          <GalleryCards/>
         </Grid.Column>
 
       </Grid>
